f81d44fb520534d05fd90a38adc5d49cfbc94acb
justkushhere <goggle.assistant29@gmail.com>
2021-09-11 21:01:23 +0800

    11th september 2021.
    Front-end: 80%
    Back-end: 10%

--- templates/main.py
+++ templates/main.py
@@ -25,11 +25,34 @@ def login():
     if logindata[request.form.get("emailID")] == request.form.get("masterkeyID"):
       app.secret_key = request.form.get("emailID")
       session['emailID']=request.form.get("emailID")
-      return render_template("securesites.html")
+      return render_template("password-display.html")
     else:
-      return render_template("securesites.html")
+      return render_template("Sign-In.html")
   else:
     logindata[request.form.get("emailID")] = request.form.get("masterkeyID")
     with open("user.json", "w") as userfile:
       json.dump(logindata, userfile)
     return render_template("login.html")
+
+
+@app.route('/signup', methods = ['GET' , 'POST'])
+def signup():
+  logindata = {}
+  if os.path.exists("user.json"):
+    with open("user.json") as userfile:
+      logindata = json.load(userfile)
+  if request.form.get("masterkeyID") == request.form.get("confirmmasterkeyID"):
+      if request.form.get("Login"):
+        if logindata[request.form.get("emailID")] == request.form.get("masterkeyID"):
+          app.secret_key = request.form.get("emailID")
+          session['emailID']=request.form.get("emailID")
+          return render_template("password-display.html")
+        else:
+          return render_template("Sign-In.html")
+      else:
+        logindata[request.form.get("emailID")] = request.form
+        with open("user.json", "w") as userfile:
+          json.dump(logindata, userfile)
+        return render_template("pasword-display.html")
+   else:
+       return render_template("Sign-Up.html")
